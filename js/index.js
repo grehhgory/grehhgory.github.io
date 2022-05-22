24710802a6c7e22024a7bf4c6a777056705c3b0c
Gregory Lim <49094805+grehhgory@users.noreply.github.com>
2022-05-22 12:11:20 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,26 +1,32 @@
-var bg_w;
+$("body").hide();
 
-if (screen.width <= 960) bg_w = 960;
-else if (screen.width > 960 && screen.width <= 1920) bg_w = 1920;
-else if (screen.width > 1920) bg_w = 3840;
+window.addEventListener("load", function () {
+	var bg_w;
 
-$(".background").attr("src", "assets/carousel-0" + "-" + bg_w + ".png");
+	if (screen.width <= 960) bg_w = 960;
+	else if (screen.width > 960 && screen.width <= 1920) bg_w = 1920;
+	else if (screen.width > 1920) bg_w = 3840;
 
-$(".carousel").flickity({
-	freeScroll: false,
-	draggable: true,
-	dragThreshold: 1, // default: 3(px)
-	prevNextButtons: false,
-	pageDots: false,
-	percentPosition: true,
-	cellAlign: "center",
-});
+	$(".background").attr("src", "assets/carousel-0" + "-" + bg_w + ".png");
 
-$(".carousel").on("change.flickity", function (event, index) {
-	console.log("Slide changed to " + index);
-	$(".background")
-		.hide()
-		.attr("src", "assets/carousel-" + index + "-" + bg_w + ".png")
-		.show();
-	console.log(bg_w);
+	$("body").fadeIn("fast");
+
+	$(".carousel").flickity({
+		freeScroll: false,
+		draggable: true,
+		dragThreshold: 1, // default: 3(px)
+		prevNextButtons: false,
+		pageDots: false,
+		percentPosition: true,
+		cellAlign: "center",
+	});
+
+	$(".carousel").on("change.flickity", function (event, index) {
+		console.log("Slide changed to " + index);
+		$(".background")
+			.hide()
+			.attr("src", "assets/carousel-" + index + "-" + bg_w + ".png")
+			.show();
+		console.log(bg_w);
+	});
 });
